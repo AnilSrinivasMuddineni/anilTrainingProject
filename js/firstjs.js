611278eef46770f3248f88299a1ea8368efc1106
AnilSrinivasMuddineni <AnilSrinivasMuddineni@gmail.com>
2017-10-19 02:49:00 +0530

    jsupdate1

--- js/firstjs.js
+++ js/firstjs.js
@@ -9,4 +9,38 @@
 		alert("you clicked for : " +serImg);
 }
                 
- 	
+ 	function clickedGmail() {
+		alert("you clicked for Gmail");
+}
+
+	function clickedMsg() {
+		alert("you clicked for Messages");
+}
+
+	function clickedGplus() {
+		alert("you clicked for Googleplus");
+}
+              
+ 	function clickedServices() {
+		alert("you clicked for Services");
+}
+
+	function clickedGuide() {
+		alert("you clicked for Guide");
+}
+
+	function clickedContacts() {
+		alert("you clicked for Contacts");
+}
+              
+ 	function clickedInf() {
+		alert("you clicked for Information");
+}
+
+	function clickedNews() {
+		alert("you clicked for Messages");
+}
+
+function clickedServ() {
+		alert("you clicked for services");
+}
