ae635ffef831a4e3673f0ccb87620387bc20e810
AnilSrinivasMuddineni <AnilSrinivasMuddineni@gmail.com>
2017-10-19 02:28:50 +0530

    jsAdded

---
+++ js/firstjs.js
@@ -0,0 +1,12 @@
+
+		function mySearch()  {
+               var ser=document.getElementById('searchBar').value
+  		alert("You Clicked for ..!!! " +ser);
+           }
+               
+		function mySearchImages() {
+			var serImg=document.getElementById("searchBar").value
+		alert("you clicked for : " +serImg);
+}
+                
+ 	
